181911640b200339a2b7cc76e5e3d891bc3f7180
Brennan Malone <malone.brennan98@gmail.com>
2023-01-10 21:49:28 -0500

    followed lecture, deploying changes

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import './HornedBeast.css';
+import Button from 'react-bootstrap/Button';
 
-class HornedBeast extends React.Component{
-  render(){
+class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      waves: 0,
+      helpMe: false,
+    }
+  }
+
+  handleWave = () => {
+    this.setState({
+      waves: this.state.waves + 1
+    })
+  }
+  needHelp = () => {
+    this.setState({
+      helpMe: true
+    });
+  }
+  gotHelp = () => {
+    this.setState({
+      helpMe: false
+    });
+  }
+  render() {
     return (
       <>
-      <h2>{this.props.title}</h2>
-      <img src='{this.props.imageURL}' alt='image of a horned beast' title='{this.props.title}'></img>
-      <p>{this.props.description}</p>
+        <article>
+          <h3>{this.props.title}</h3>
+          <p>Greetings</p>
+          <p onClick={this.handleWave}> Hi {this.state.waves} {this.props.description}</p>
+          <img alt="the beast" src={this.props.imageURL} />
+          <Button onClick={this.needHelp}>I need help</Button>
+          <Button onClick={this.gotHelp}>I got helped</Button>
+          <div>{this.state.helpMe ? 'I need Help' : ''}</div>
+        </article>
       </>
     )
   }
